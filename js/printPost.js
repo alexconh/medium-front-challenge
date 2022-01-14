c56ba2d4100c917cd1f3f4d8ca6d9779cdd4d71e
EMMMED <personal.emedina@gmail.com>
2022-01-13 21:17:28 -0600

    href directed & set timeout

---
+++ js/printPost.js
@@ -0,0 +1,92 @@
+const getPost = () => {
+    // GET - default
+    fetch("https://medium-challenge-default-rtdb.firebaseio.com/post/.json")
+    .then((res) => {
+    return res.json()
+    })
+    .then((posts) => {
+    console.log(posts)
+    
+    // console.log(Object.values(posts)) Investigar
+    
+    let postsLayout = ''
+
+    let orderedPosts = Object.keys(posts).reverse()
+    console.log(orderedPosts)
+
+    
+    for(post of orderedPosts) {
+        let { title, timetoread, resume, author, creationdate, primaryimg, topic } = posts[post]
+        postsLayout += `
+            <article class="card mb-5">
+                <!-- CARD -->
+                <!-- user avatar and creation date -->
+                <div class="card-header d-flex justify-content-start gap-2 align-items-center mb-3 p-0">
+                    <img class="card-avatar rounded-circle" src="https://i.pravatar.cc/40" alt="">
+                    <p class="card-author m-0">${author}</p>
+                    <p class="card-date m-0 text-muted"> · ${creationdate}</p>
+                </div>
+                <!-- title, resume and image -->
+                <div class="card-body d-flex justify-content-between p-0 mb-4">
+                    <div class="card-body-text">
+                    <h5 class="card-title fw-bolder m-0">${title}</h5>
+                    <p>${resume.slice(0,200)} ...</p>
+                    </div>
+                    
+                    <a href="inside-post.html?idpost=${post}">
+                    <div class="card-body-img ms-4" style="background-image: url(${primaryimg});">
+                    
+                    </div>
+                    </a>
+
+                </div>
+                <div class="card-bottom mb-4 d-flex align-items-center justify-content-between">
+                    <!-- topic and time to read -->
+                    <div class="d-flex align-items-center">
+                    <button type="button" class="btn btn-sm rounded-pill d-flex align-items-center">${topic}</button>
+                    <p class="card-read m-0 mx-2 text-muted">${timetoread}</p>
+                    </div>  
+                    <div class="card-bottom-icons d-flex align-items-center">
+                    <!-- save and dropdown menu -->
+                    <input type="checkbox" name="save-card" class="save" id="save-${post}">
+                    <label for="save-${post}">
+                        <svg width="25" height="25" viewBox="0 0 25 25" fill="none" class="ne">
+                        <path class="save-icon" d="M18 2.5a.5.5 0 0 1 1 0V5h2.5a.5.5 0 0 1 0 1H19v2.5a.5.5 0 1 1-1 0V6h-2.5a.5.5 0 0 1 0-1H18V2.5zM7 7a1 1 0 0 1 1-1h3.5a.5.5 0 0 0 0-1H8a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V7z" fill="#292929"></path>
+                        <path
+                        d="M4.5 6.25V21c0 .2.24.32.4.2l5.45-4.09a.25.25 0 0 1 .3 0l5.45 4.09c.16.12.4 0 .4-.2V6.25a.25.25 0 0 0-.25-.25H4.75a.25.25 0 0 0-.25.25z"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        class="save-icon2"
+                        ></path>
+                        </svg>
+                    </label>
+                    <div class="dropdown ms-2">
+                        <!-- dropdown -->
+                        <button class="btn-dark p-0" type="button" id="dropdownMenuButton1" data-bs-offset="-69,5" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg class="overflow-dots-filled-25px_svg__svgIcon-use" width="25" height="25">
+                            <path d="M5 12.5c0 .55.2 1.02.59 1.41.39.4.86.59 1.41.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41A1.93 1.93 0 0 0 7 10.5c-.55 0-1.02.2-1.41.59-.4.39-.59.86-.59 1.41zm5.62 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.42.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41a1.93 1.93 0 0 0-1.41-.59c-.55 0-1.03.2-1.42.59-.39.39-.58.86-.58 1.41zm5.6 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.43.59.56 0 1.03-.2 1.42-.59.39-.39.58-.86.58-1.41 0-.55-.2-1.02-.58-1.41a1.93 1.93 0 0 0-1.42-.59c-.56 0-1.04.2-1.43.59-.39.39-.58.86-.58 1.41z" fill-rule="evenodd"></path>
+                        </svg>
+                        </button>                 
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">
+                            <svg width="19" height="19" viewBox="0 0 23 23" fill="none"><path d="M6.2 16.8A7.5 7.5 0 1 0 16.8 6.2 7.5 7.5 0 0 0 6.2 16.8z" stroke="#000" stroke-width="2" stroke-linecap="round"></path><path d="M6 6l11 11" stroke="#000" stroke-width="2"></path></svg>
+                            Show less like this</a></li>
+                            <li class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Mute this author</a></li>
+                        <li><a class="dropdown-item" href="#">Mute this publication</a></li>
+                        <li><a class="dropdown-item" href="#">Report</a></li>
+                        </ul>
+                    </div>
+                    <div class="card-bottom-spacer ms-4" style="width:110px;"></div>
+                    </div>
+                </div>
+            </article>
+        `
+    }
+
+    document.querySelector('#cards-container').innerHTML = postsLayout
+
+    })
+    }    
+
+    getPost()
--- pruebas.js
+++ pruebas.js
@@ -17,36 +17,68 @@ const getPost = () => {
     
 
     for(post of orderedPosts) {
-        let { title, timetoread, resume, author, creationdate, primaryimg } = posts[post]
+        let { title, timetoread, resume, author, creationdate, primaryimg, topic } = posts[post]
         postsLayout += `
-        <div class="card col-12">
-            <div class="card-header d-flex row justify-content-start align-items-center gap-3 m-0">
-              <div class="card-img-container col-2 ">
-                <img class="card-avatar rounded-circle" src="https://i.pravatar.cc/40" alt="">
-              </div>
-              <div class="card-author-container col-8 p-s-5">
-                <h5 class="card-author m-0">${author}</h5>
-                <p class="card-date m-0 mb-2 text-muted">${creationdate}</p>
-              </div>
-            </div>
-            <div class="card-body row align-items-center">
-              <h6 class="card-title m-0 col-9 fs-3">${resume}</h6>
-              <div class="col-2 p-0">
-                <a href="inside-post.html?idpost=${post}"><img class="card-img img-fluid max-width: 100%;" src="${primaryimg}" alt=""></a>
-              </div>
-            </div>
-            <div class="card-bottom row p-3 py-0">
-              <div class="card-settings row">
-                <p class="card-category col-3 p-1">Health</p>
-                <p class="card-read col-3 p-1">${timetoread}</p>
-                <div class="col-2 p-1">
-                  <svg class="save-icon" width="25" height="25" viewBox="0 0 25 25" fill="none" class="ne"><path d="M18 2.5a.5.5 0 0 1 1 0V5h2.5a.5.5 0 0 1 0 1H19v2.5a.5.5 0 1 1-1 0V6h-2.5a.5.5 0 0 1 0-1H18V2.5zM7 7a1 1 0 0 1 1-1h3.5a.5.5 0 0 0 0-1H8a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V7z" fill="#292929"></path></svg>
+        
+        <article class="card mb-5">
+                <!-- CARD -->
+                <!-- user avatar and creation date -->
+                <div class="card-header d-flex justify-content-start gap-2 align-items-center mb-3 p-0">
+                    <img class="card-avatar rounded-circle" src="https://i.pravatar.cc/40" alt="">
+                    <p class="card-author m-0">${author}</p>
+                    <p class="card-date m-0 text-muted"> · ${creationdate}</p>
                 </div>
-                <div class="col-2 p-1">
-                  <svg class="overflow-dots-filled-25px_svg__svgIcon-use" width="25" height="25"><path d="M5 12.5c0 .55.2 1.02.59 1.41.39.4.86.59 1.41.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41A1.93 1.93 0 0 0 7 10.5c-.55 0-1.02.2-1.41.59-.4.39-.59.86-.59 1.41zm5.62 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.42.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41a1.93 1.93 0 0 0-1.41-.59c-.55 0-1.03.2-1.42.59-.39.39-.58.86-.58 1.41zm5.6 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.43.59.56 0 1.03-.2 1.42-.59.39-.39.58-.86.58-1.41 0-.55-.2-1.02-.58-1.41a1.93 1.93 0 0 0-1.42-.59c-.56 0-1.04.2-1.43.59-.39.39-.58.86-.58 1.41z" fill-rule="evenodd"></path></svg>
+                <!-- title, resume and image -->
+                <div class="card-body d-flex justify-content-between p-0 mb-4">
+                    <div class="card-body-text">
+                    <h5 class="card-title fw-bolder m-0">${title}</h5>
+                    <p>${resume.slice(200)}...</p>
+                    </div>
+                    <div class="card-body-img ms-4" style="background-image: ${primaryimg};">
+                        <a href="inside-post.html?idpost=${post}"></a>
+                    </div>
                 </div>
-              </div>
-          </div>
+                <div class="card-bottom mb-4 d-flex align-items-center justify-content-between">
+                    <!-- topic and time to read -->
+                    <div class="d-flex align-items-center">
+                    <button type="button" class="btn btn-sm rounded-pill d-flex align-items-center">${topic}</button>
+                    <p class="card-read m-0 mx-2 text-muted">${timetoread}</p>
+                    </div>  
+                    <div class="card-bottom-icons d-flex align-items-center">
+                    <!-- save and dropdown menu -->
+                    <input type="checkbox" name="save-card" id="save">
+                    <label for="save">
+                        <svg width="25" height="25" viewBox="0 0 25 25" fill="none" class="ne">
+                        <path class="save-icon" d="M18 2.5a.5.5 0 0 1 1 0V5h2.5a.5.5 0 0 1 0 1H19v2.5a.5.5 0 1 1-1 0V6h-2.5a.5.5 0 0 1 0-1H18V2.5zM7 7a1 1 0 0 1 1-1h3.5a.5.5 0 0 0 0-1H8a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V7z" fill="#292929"></path>
+                        <path
+                        d="M4.5 6.25V21c0 .2.24.32.4.2l5.45-4.09a.25.25 0 0 1 .3 0l5.45 4.09c.16.12.4 0 .4-.2V6.25a.25.25 0 0 0-.25-.25H4.75a.25.25 0 0 0-.25.25z"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        class="save-icon2"
+                        ></path>
+                        </svg>
+                    </label>
+                    <div class="dropdown ms-2">
+                        <!-- dropdown -->
+                        <button class="btn-dark p-0" type="button" id="dropdownMenuButton1" data-bs-offset="-71,5" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg class="overflow-dots-filled-25px_svg__svgIcon-use" width="25" height="25">
+                            <path d="M5 12.5c0 .55.2 1.02.59 1.41.39.4.86.59 1.41.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41A1.93 1.93 0 0 0 7 10.5c-.55 0-1.02.2-1.41.59-.4.39-.59.86-.59 1.41zm5.62 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.42.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41a1.93 1.93 0 0 0-1.41-.59c-.55 0-1.03.2-1.42.59-.39.39-.58.86-.58 1.41zm5.6 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.43.59.56 0 1.03-.2 1.42-.59.39-.39.58-.86.58-1.41 0-.55-.2-1.02-.58-1.41a1.93 1.93 0 0 0-1.42-.59c-.56 0-1.04.2-1.43.59-.39.39-.58.86-.58 1.41z" fill-rule="evenodd"></path>
+                        </svg>
+                        </button>                 
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">
+                            <svg width="19" height="19" viewBox="0 0 23 23" fill="none"><path d="M6.2 16.8A7.5 7.5 0 1 0 16.8 6.2 7.5 7.5 0 0 0 6.2 16.8z" stroke="#000" stroke-width="2" stroke-linecap="round"></path><path d="M6 6l11 11" stroke="#000" stroke-width="2"></path></svg>
+                            Show less like this</a></li>
+                            <li class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Mute this author</a></li>
+                        <li><a class="dropdown-item" href="#">Mute this publication</a></li>
+                        <li><a class="dropdown-item" href="#">Report</a></li>
+                        </ul>
+                    </div>
+                    <div class="card-bottom-spacer ms-4" style="width:110px;"></div>
+                    </div>
+                </div>
+            </article>
 
         `
     }
@@ -87,7 +119,34 @@ window.addEventListener('load', () => {
 
 
     /*
-    
+     <div class="card col-12">
+            <div class="card-header d-flex row justify-content-start align-items-center gap-3 m-0">
+              <div class="card-img-container col-2 ">
+                <img class="card-avatar rounded-circle" src="https://i.pravatar.cc/40" alt="">
+              </div>
+              <div class="card-author-container col-8 p-s-5">
+                <h5 class="card-author m-0">${author}</h5>
+                <p class="card-date m-0 mb-2 text-muted">${creationdate}</p>
+              </div>
+            </div>
+            <div class="card-body row align-items-center">
+              <h6 class="card-title m-0 col-9 fs-3">${resume}</h6>
+              <div class="col-2 p-0">
+                <a href="inside-post.html?idpost=${post}"><img class="card-img img-fluid max-width: 100%;" src="${primaryimg}" alt=""></a>
+              </div>
+            </div>
+            <div class="card-bottom row p-3 py-0">
+              <div class="card-settings row">
+                <p class="card-category col-3 p-1">Health</p>
+                <p class="card-read col-3 p-1">${timetoread}</p>
+                <div class="col-2 p-1">
+                  <svg class="save-icon" width="25" height="25" viewBox="0 0 25 25" fill="none" class="ne"><path d="M18 2.5a.5.5 0 0 1 1 0V5h2.5a.5.5 0 0 1 0 1H19v2.5a.5.5 0 1 1-1 0V6h-2.5a.5.5 0 0 1 0-1H18V2.5zM7 7a1 1 0 0 1 1-1h3.5a.5.5 0 0 0 0-1H8a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V7z" fill="#292929"></path></svg>
+                </div>
+                <div class="col-2 p-1">
+                  <svg class="overflow-dots-filled-25px_svg__svgIcon-use" width="25" height="25"><path d="M5 12.5c0 .55.2 1.02.59 1.41.39.4.86.59 1.41.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41A1.93 1.93 0 0 0 7 10.5c-.55 0-1.02.2-1.41.59-.4.39-.59.86-.59 1.41zm5.62 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.42.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41a1.93 1.93 0 0 0-1.41-.59c-.55 0-1.03.2-1.42.59-.39.39-.58.86-.58 1.41zm5.6 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.43.59.56 0 1.03-.2 1.42-.59.39-.39.58-.86.58-1.41 0-.55-.2-1.02-.58-1.41a1.93 1.93 0 0 0-1.42-.59c-.56 0-1.04.2-1.43.59-.39.39-.58.86-.58 1.41z" fill-rule="evenodd"></path></svg>
+                </div>
+              </div>
+          </div>
     
     */
 
